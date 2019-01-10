1f5da676a37cdec358b3dbf645bcb02a27bb2af8
liciolentimo <lentimo@gmail.com>
2019-01-10 08:17:10 +0300

    Added custom pipe datetime

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatetimePipe } from './datetime.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatetimePipe
   ],
   imports: [
     BrowserModule,
--- src/app/diary.ts
+++ src/app/diary.ts
@@ -1,2 +1,7 @@
 export class Diary {
+   
+    onAdd(){
+    
+    }
 }
+
